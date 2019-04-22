16f31187bd0e0e88fcd692d9927329e1078f506e
Louis-Maxime Crédeville <credeville.louis.maxime@gmail.com>
2019-04-22 16:08:28 +0200

    test ResultSetToComputerMapper

---
+++ src/test/java/com/excilys/cdb/mapper/resultset/ResultSetToComputerMapperTest.java
@@ -0,0 +1,13 @@
+package com.excilys.cdb.mapper.resultset;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ResultSetToComputerMapperTest {
+
+    @Test
+    void map() {
+        fail();
+    }
+}
