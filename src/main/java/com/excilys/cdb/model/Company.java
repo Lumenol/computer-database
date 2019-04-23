d499fbca4c461611a8e33554e3d7ff992028fb25
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-04-23 15:27:57 +0200

    test ResultSetToComputerMapper

--- src/main/java/com/excilys/cdb/model/Company.java
+++ src/main/java/com/excilys/cdb/model/Company.java
@@ -8,63 +8,81 @@ public class Company {
     private final String name;
 
     private Company(Long id, String name) {
-        Objects.requireNonNull(name);
-        this.id = id;
-        this.name = name;
+	Objects.requireNonNull(name);
+	this.id = id;
+	this.name = name;
     }
 
     public static CompagnyBuilder builder() {
-        return new CompagnyBuilder();
+	return new CompagnyBuilder();
     }
 
     public Long getId() {
-        return id;
+	return id;
     }
 
     public String getName() {
-        return name;
+	return name;
     }
 
     @Override
     public String toString() {
-        return "Compagny [id=" + id + ", name=" + name + "]";
+	return "Compagny [id=" + id + ", name=" + name + "]";
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Company company = (Company) o;
-        return Objects.equals(id, company.id);
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Company other = (Company) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	return result;
     }
 
     public static class CompagnyBuilder {
 
-        private Long id;
+	private Long id;
 
-        private String name;
+	private String name;
 
-        private CompagnyBuilder() {
-        }
+	private CompagnyBuilder() {
+	}
 
-        public Company build() {
-            return new Company(id, name);
-        }
+	public Company build() {
+	    return new Company(id, name);
+	}
 
-        public CompagnyBuilder id(long id) {
-            this.id = id;
-            return this;
-        }
+	public CompagnyBuilder id(long id) {
+	    this.id = id;
+	    return this;
+	}
 
-        public CompagnyBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
+	public CompagnyBuilder name(String name) {
+	    this.name = name;
+	    return this;
+	}
 
     }
 
--- src/test/java/com/excilys/cdb/mapper/resultset/ResultSetToComputerMapperTest.java
+++ src/test/java/com/excilys/cdb/mapper/resultset/ResultSetToComputerMapperTest.java
@@ -1,13 +1,75 @@
 package com.excilys.cdb.mapper.resultset;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.model.Computer;
+import com.excilys.cdb.model.Computer.ComputerBuilder;
 
 class ResultSetToComputerMapperTest {
 
-    @Test
-    void map() {
-        fail();
+    private static final String COLUMN_COMPANY_ID = "company_id";
+    private static final String COLUMN_COMPANY_NAME = "company_name";
+    private static final String COLUMN_DISCONTINUED = "discontinued";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_INTRODUCED = "introduced";
+    private static final String COLUMN_NAME = "name";
+
+    private ResultSet mockResultSet;
+
+    @BeforeEach
+    public void mockResultSet() {
+	mockResultSet = Mockito.mock(ResultSet.class);
+    };
+
+    private static Stream<Arguments> provideComputer() {
+	return Stream.of(Arguments.of(1l, "Le premier", null, null, 4l, "4eme corp"),
+		Arguments.of(2l, "Le dexieme", LocalDate.of(2017, 10, 10), null, null, null),
+		Arguments.of(3l, "Le 3eme", LocalDate.of(2016, 9, 9), LocalDate.of(2017, 10, 10), 5l, "Une autre corp"),
+		Arguments.of(4l, "Le 4eme", null, LocalDate.of(2017, 10, 10), 6l, "La 6eme"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideComputer")
+    public void map(long id, String name, LocalDate introduced, LocalDate discontinued, Long companyId,
+	    String companyName) throws SQLException {
+	Mockito.when(mockResultSet.getLong(COLUMN_ID)).thenReturn(id);
+	Mockito.when(mockResultSet.getString(COLUMN_NAME)).thenReturn(name);
+
+	Date introducedSQL = Objects.nonNull(introduced) ? Date.valueOf(introduced) : null;
+	Mockito.when(mockResultSet.getDate(COLUMN_INTRODUCED)).thenReturn(introducedSQL);
+
+	Date discontinuedSQL = Objects.nonNull(discontinued) ? Date.valueOf(discontinued) : null;
+	Mockito.when(mockResultSet.getDate(COLUMN_DISCONTINUED)).thenReturn(discontinuedSQL);
+
+	long companyIdSQL = Objects.nonNull(companyId) ? companyId : 0;
+	Mockito.when(mockResultSet.getLong(COLUMN_COMPANY_ID)).thenReturn(companyIdSQL);
+
+	Mockito.when(mockResultSet.getString(COLUMN_COMPANY_NAME)).thenReturn(companyName);
+
+	Computer computer = ResultSetToComputerMapper.getInstance().map(mockResultSet);
+
+	ComputerBuilder computerBuilder = Computer.builder().id(id).name(name).introduced(introduced)
+		.discontinued(discontinued);
+	if (Objects.nonNull(companyId)) {
+	    Company company = Company.builder().id(companyId).name(companyName).build();
+	    computerBuilder.manufacturer(company);
+	}
+	Computer expectedComputer = computerBuilder.build();
+
+	assertEquals(expectedComputer, computer);
     }
 }
