0d7f9fb401d78f4dd3f6ccb04a45fe65ae40bd44
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-04-24 15:31:54 +0200

    test findAll ComputerDAO

--- src/test/java/com/excilys/cdb/dao/ComputerDaoTest.java
+++ src/test/java/com/excilys/cdb/dao/ComputerDaoTest.java
@@ -4,7 +4,9 @@ import com.excilys.cdb.model.Computer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -18,54 +20,60 @@ import static org.junit.jupiter.api.Assertions.fail;
 class ComputerDaoTest {
 
     private static Stream<Long> provideComputerId() {
-        final Stream.Builder<Long> builder = Stream.builder();
-        final List<Computer> allComputers = TestDatabase.getInstance().findAllComputers();
-        final Computer lastComputers = allComputers.get(allComputers.size() - 1);
-        for (long i = -5; i < lastComputers.getId() + 5; i++) {
-            builder.add(i);
-        }
-        return builder.build();
+	final Stream.Builder<Long> builder = Stream.builder();
+	final List<Computer> allComputers = TestDatabase.getInstance().findAllComputers();
+	final Computer lastComputers = allComputers.get(allComputers.size() - 1);
+	for (long i = -5; i < lastComputers.getId() + 5; i++) {
+	    builder.add(i);
+	}
+	return builder.build();
     }
 
     @BeforeEach
     public void loadEnttries() throws IOException, SQLException {
-        TestDatabase.getInstance().reload();
+	TestDatabase.getInstance().reload();
     }
 
     @Test
     void create() {
-        fail();
+	fail();
 
     }
 
     @Test
     void deleteById() {
-        fail();
+	fail();
 
     }
 
-    @Test
-    void findAll() {
-        fail();
+    private static Stream<Arguments> provideOffsetLimit() {
+	return Stream.of(Arguments.of(0, 30), Arguments.of(0, 5), Arguments.of(5, 5), Arguments.of(13, 25));
+    }
 
+    @ParameterizedTest
+    @MethodSource("provideOffsetLimit")
+    void findAll(long offset, long limit) {
+	final List<Computer> actual = ComputerDao.getInstance().findAll(offset, limit);
+	final List<Computer> expected = TestDatabase.getInstance().findAllComputers(offset,limit);
+	assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @MethodSource("provideComputerId")
     void findById(long id) {
-        final Optional<Computer> expected = Optional.ofNullable(TestDatabase.getInstance().findComputerById(id));
-        final Optional<Computer> actual = ComputerDao.getInstance().findById(id);
-        assertEquals(expected, actual);
+	final Optional<Computer> expected = Optional.ofNullable(TestDatabase.getInstance().findComputerById(id));
+	final Optional<Computer> actual = ComputerDao.getInstance().findById(id);
+	assertEquals(expected, actual);
     }
 
     @Test
     void update() {
-        fail();
+	fail();
     }
 
     @Test
     void count() {
-        final long count = ComputerDao.getInstance().count();
-        assertEquals(TestDatabase.getInstance().findAllComputers().size(), count);
+	final long count = ComputerDao.getInstance().count();
+	assertEquals(TestDatabase.getInstance().findAllComputers().size(), count);
     }
 }
