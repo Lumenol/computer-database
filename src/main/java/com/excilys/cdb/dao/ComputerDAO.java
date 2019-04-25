e1c8bc52bb6153fdf32aafced20f9ac6a2026ae9
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-04-25 10:49:19 +0200

    logger ComputerDAO

--- src/main/java/com/excilys/cdb/dao/ComputerDAO.java
+++ src/main/java/com/excilys/cdb/dao/ComputerDAO.java
@@ -12,6 +12,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static com.excilys.cdb.dao.DAOUtils.haveOneOrEmpty;
 
 public class ComputerDAO {
@@ -25,71 +28,80 @@ public class ComputerDAO {
 
     private static ComputerDAO instance;
     private final ConnectionProvider connectionProvider = ConnectionProvider.getInstance();
-    private final ResultSetMapper<List<Computer>> resultSetMapper = new ResultSetToListMapper<>(ResultSetToComputerMapper.getInstance());
+    private final ResultSetMapper<List<Computer>> resultSetMapper = new ResultSetToListMapper<>(
+	    ResultSetToComputerMapper.getInstance());
     private final ResultSetToCountMapper resultSetToCountMapper = ResultSetToCountMapper.getInstance();
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private ComputerDAO() {
     }
 
     public static ComputerDAO getInstance() {
-        if (Objects.isNull(instance)) {
-            instance = new ComputerDAO();
-        }
-        return instance;
+	if (Objects.isNull(instance)) {
+	    instance = new ComputerDAO();
+	}
+	return instance;
     }
 
     public long create(Computer computer) {
-        final SQLComputer sqlComputer = SQLComputer.from(computer);
-        try {
-            return JDBCUtils.insert(connectionProvider, SQL_CREATE, sqlComputer.getName(), sqlComputer.getIntroduced(), sqlComputer.getDiscontinued(), sqlComputer.getManufacturerId());
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	final SQLComputer sqlComputer = SQLComputer.from(computer);
+	try {
+	    return JDBCUtils.insert(connectionProvider, SQL_CREATE, sqlComputer.getName(), sqlComputer.getIntroduced(),
+		    sqlComputer.getDiscontinued(), sqlComputer.getManufacturerId());
+	} catch (SQLException e) {
+	    logger.warn("create(" + computer + ")", e);
+	    throw new ComputerDAOException(e);
+	}
 
     }
 
     public void deleteById(long id) {
-        try {
-            JDBCUtils.delete(connectionProvider, SQL_DELETE, id);
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	try {
+	    JDBCUtils.delete(connectionProvider, SQL_DELETE, id);
+	} catch (SQLException e) {
+	    logger.warn("deleteById(" + id + ")", e);
+	    throw new ComputerDAOException(e);
+	}
     }
 
     public List<Computer> findAll(long offset, long limit) {
-        try {
-            return JDBCUtils.find(resultSetMapper, connectionProvider, SQL_FIND_ALL_PAGED, limit,
-                    offset);
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	try {
+	    return JDBCUtils.find(resultSetMapper, connectionProvider, SQL_FIND_ALL_PAGED, limit, offset);
+	} catch (SQLException e) {
+	    logger.warn("findAll(" + offset + "," + limit + ")", e);
+	    throw new ComputerDAOException(e);
+	}
     }
 
     public Optional<Computer> findById(long id) {
-        try {
-            List<Computer> computers = JDBCUtils.find(resultSetMapper, connectionProvider, SQL_FIND_BY_ID,
-                    id);
-            return haveOneOrEmpty(computers);
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	try {
+	    List<Computer> computers = JDBCUtils.find(resultSetMapper, connectionProvider, SQL_FIND_BY_ID, id);
+	    return haveOneOrEmpty(computers);
+	} catch (SQLException e) {
+	    logger.warn("findById(" + id + ")", e);
+	    throw new ComputerDAOException(e);
+	}
     }
 
     public void update(Computer computer) {
-        final SQLComputer sqlComputer = SQLComputer.from(computer);
-        try {
-            JDBCUtils.update(connectionProvider, SQL_UPDATE, sqlComputer.getName(), sqlComputer.getIntroduced(), sqlComputer.getDiscontinued(), sqlComputer.getManufacturerId(), sqlComputer.getId());
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	final SQLComputer sqlComputer = SQLComputer.from(computer);
+	try {
+	    JDBCUtils.update(connectionProvider, SQL_UPDATE, sqlComputer.getName(), sqlComputer.getIntroduced(),
+		    sqlComputer.getDiscontinued(), sqlComputer.getManufacturerId(), sqlComputer.getId());
+	} catch (SQLException e) {
+	    logger.warn("update(" + computer + ")", e);
+	    throw new ComputerDAOException(e);
+	}
 
     }
 
     public long count() {
-        try {
-            return JDBCUtils.find(resultSetToCountMapper, connectionProvider, SQL_COUNT);
-        } catch (SQLException e) {
-            throw new ComputerDAOException(e);
-        }
+	try {
+	    return JDBCUtils.find(resultSetToCountMapper, connectionProvider, SQL_COUNT);
+	} catch (SQLException e) {
+	    logger.warn("count()", e);
+	    throw new ComputerDAOException(e);
+	}
     }
 }
