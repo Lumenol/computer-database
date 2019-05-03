83b1e08b6888be47e77595b88fb03f71d98dae91
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-05-03 16:45:15 +0200

    Ajout de HickariCP, renomage de database.properties en datasource.properties

--- src/main/java/com/excilys/cdb/dao/ConnectionProvider.java
+++ src/main/java/com/excilys/cdb/dao/ConnectionProvider.java
@@ -1,56 +1,37 @@
 package com.excilys.cdb.dao;
 
-import com.excilys.cdb.exception.DriverNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
-import java.util.ResourceBundle;
 import java.util.TimeZone;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionProvider {
 
+    private static final String DATASOURCE_PROPERTIES = "/datasource.properties";
+    private static final TimeZone TIME_ZONE_UTC = TimeZone.getTimeZone("UTC");
     private static ConnectionProvider instance;
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-    private String password;
-    private String url;
-    private String user;
+    private final DataSource dataSource;
 
-    private ConnectionProvider(String url, String user, String password, String driver) {
-        super();
-        this.url = url;
-        this.user = user;
-        this.password = password;
-
-        try {
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) {
-            logger.warn("Driver {} not found.", driver);
-            throw new DriverNotFoundException(e);
-        }
-    }
-
-    private static ResourceBundle bundle() {
-        return ResourceBundle.getBundle("database");
+    private ConnectionProvider(DataSource dataSource) {
+	this.dataSource = dataSource;
     }
 
     public static ConnectionProvider getInstance() {
-        if (Objects.isNull(instance)) {
-            TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-            ResourceBundle bundle = bundle();
-            final String url = bundle.getString("url");
-            final String username = bundle.getString("username");
-            final String password = bundle.getString("password");
-            final String driver = bundle.getString("driver");
-            instance = new ConnectionProvider(url, username, password, driver);
-        }
-        return instance;
+	if (Objects.isNull(instance)) {
+	    TimeZone.setDefault(TIME_ZONE_UTC);
+	    HikariConfig config = new HikariConfig(DATASOURCE_PROPERTIES);
+	    final HikariDataSource dataSource = new HikariDataSource(config);
+	    instance = new ConnectionProvider(dataSource);
+	}
+	return instance;
     }
 
     public Connection get() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
+	return dataSource.getConnection();
     }
 }
