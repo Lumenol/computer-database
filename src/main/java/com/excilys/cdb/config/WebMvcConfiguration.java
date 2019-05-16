0f4b7c231f99b5232eec08cc98c465a361d99040
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-05-16 18:44:44 +0200

    configuration de spring-webmvc

---
+++ src/main/java/com/excilys/cdb/config/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.excilys.cdb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+}
