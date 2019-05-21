c90707638841aaa69d1459c94398fe506e7ef256
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-05-21 14:17:53 +0200

    edit computer

--- src/main/java/com/excilys/cdb/controller/web/EditComputerServlet.java
+++ src/main/java/com/excilys/cdb/controller/web/EditComputerServlet.java
@@ -1,21 +1,23 @@
 package com.excilys.cdb.controller.web;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.excilys.cdb.dto.CompanyDTO;
 import com.excilys.cdb.dto.UpdateComputerDTO;
-import com.excilys.cdb.mapper.MapperUtils;
+import com.excilys.cdb.exception.BadArgumentRequestException;
 import com.excilys.cdb.mapper.dto.CompanyToCompanyDTOMapper;
 import com.excilys.cdb.mapper.dto.ComputerToUpdateComputerDTOMapper;
 import com.excilys.cdb.mapper.dto.UpdateComputerDTOToComputerMapper;
@@ -24,115 +26,63 @@ import com.excilys.cdb.service.CompanyService;
 import com.excilys.cdb.service.ComputerService;
 import com.excilys.cdb.validator.UpdateComputerValidator;
 
-public class EditComputerServlet extends HttpServlet {
-    private static final String PARAMETER_ERRORS = "errors";
+@Controller
+@RequestMapping("/computers/edit/{id}")
+public class EditComputerServlet {
     private static final String PARAMETER_COMPUTER = "computer";
     private static final String PARAMETER_SUCCESS = "success";
-    private static final long serialVersionUID = 1L;
     private static final String PARAMETER_COMPANIES = "companies";
-    private static final String EDIT_COMPUTER_JSP = "/WEB-INF/views/editComputer.jsp";
-    private static final String PARAMETER_ID = "id";
-    private static final String PARAMETER_COMPUTER_NAME = "name";
-    private static final String PARAMETER_INTRODUCED = "introduced";
-    private static final String PARAMETER_DISCONTINUED = "discontinued";
-    private static final String PARAMETER_MANNUFACTURER_ID = "mannufacturerId";
-    private CompanyService companyService;
-    private UpdateComputerDTOToComputerMapper updateComputerDTOToComputerMapper;
-    private UpdateComputerValidator updateComputerValidator;
-    private CompanyToCompanyDTOMapper companyToCompanyDTOMapper;
-    private ComputerService computerService;
-    private ComputerToUpdateComputerDTOMapper computerToUpdateComputerDTOMapper;
+    private static final String EDIT_COMPUTER_JSP = "editComputer";
+    private final CompanyService companyService;
+    private final UpdateComputerDTOToComputerMapper updateComputerDTOToComputerMapper;
+    private final UpdateComputerValidator updateComputerValidator;
+    private final CompanyToCompanyDTOMapper companyToCompanyDTOMapper;
+    private final ComputerService computerService;
+    private final ComputerToUpdateComputerDTOMapper computerToUpdateComputerDTOMapper;
 
-    @Override
-    public void init() throws ServletException {
-	super.init();
-	final WebApplicationContext webApplicationContext = WebApplicationContextUtils
-		.getRequiredWebApplicationContext(getServletContext());
-	companyService = webApplicationContext.getBean(CompanyService.class);
-	updateComputerDTOToComputerMapper = webApplicationContext.getBean(UpdateComputerDTOToComputerMapper.class);
-	updateComputerValidator = webApplicationContext.getBean(UpdateComputerValidator.class);
-	companyToCompanyDTOMapper = webApplicationContext.getBean(CompanyToCompanyDTOMapper.class);
-	computerService = webApplicationContext.getBean(ComputerService.class);
-	computerToUpdateComputerDTOMapper = webApplicationContext.getBean(ComputerToUpdateComputerDTOMapper.class);
+    public EditComputerServlet(CompanyService companyService,
+	    UpdateComputerDTOToComputerMapper updateComputerDTOToComputerMapper,
+	    UpdateComputerValidator updateComputerValidator, CompanyToCompanyDTOMapper companyToCompanyDTOMapper,
+	    ComputerService computerService, ComputerToUpdateComputerDTOMapper computerToUpdateComputerDTOMapper) {
+	super();
+	this.companyService = companyService;
+	this.updateComputerDTOToComputerMapper = updateComputerDTOToComputerMapper;
+	this.updateComputerValidator = updateComputerValidator;
+	this.companyToCompanyDTOMapper = companyToCompanyDTOMapper;
+	this.computerService = computerService;
+	this.computerToUpdateComputerDTOMapper = computerToUpdateComputerDTOMapper;
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-
-	try {
-	    final long id = Long.parseLong(getParameterId(request));
-	    final Optional<UpdateComputerDTO> computer = computerService.findById(id)
-		    .map(computerToUpdateComputerDTOMapper::map);
-
-	    if (!computer.isPresent()) {
-		response.sendError(404);
-		return;
-	    } else {
-		setParameterComputer(request, computer.get());
-	    }
-
-	} catch (NumberFormatException e) {
-	    response.sendError(404);
-	    return;
-	}
-
-	setParameterCompanies(request);
-	forwardToJsp(request, response);
+    @InitBinder
+    protected void initBinding(WebDataBinder dataBinder) {
+	dataBinder.addValidators(updateComputerValidator);
     }
 
-    private void forwardToJsp(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	getServletContext().getRequestDispatcher(EDIT_COMPUTER_JSP).forward(request, response);
+    @ModelAttribute(PARAMETER_COMPANIES)
+    public List<CompanyDTO> companies() {
+	return companyService.findAll().stream().map(companyToCompanyDTOMapper::map).collect(Collectors.toList());
     }
 
-    private void setParameterCompanies(HttpServletRequest request) {
-	final List<CompanyDTO> companies = companyService.findAll().stream().map(companyToCompanyDTOMapper::map)
-		.collect(Collectors.toList());
-	request.setAttribute(PARAMETER_COMPANIES, companies);
+    @GetMapping
+    public ModelAndView form(@PathVariable long id) {
+	final UpdateComputerDTO computer = computerService.findById(id).map(computerToUpdateComputerDTOMapper::map)
+		.orElseThrow(BadArgumentRequestException::new);
+
+	final ModelAndView modelAndView = new ModelAndView(EDIT_COMPUTER_JSP);
+	modelAndView.addObject(PARAMETER_COMPUTER, computer);
+	return modelAndView;
     }
 
-    private void setParameterComputer(HttpServletRequest request, final UpdateComputerDTO computer) {
-	request.setAttribute(PARAMETER_COMPUTER, computer);
-    }
+    @PostMapping
+    public ModelAndView edit(@Validated @ModelAttribute(PARAMETER_COMPUTER) UpdateComputerDTO computerDTO,
+	    BindingResult result) {
 
-    private String getParameterId(HttpServletRequest request) {
-	return request.getParameter(PARAMETER_ID);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	final String id = getParameterId(request);
-	final String name = request.getParameter(PARAMETER_COMPUTER_NAME);
-	final String introduced = request.getParameter(PARAMETER_INTRODUCED);
-	final String discontinued = request.getParameter(PARAMETER_DISCONTINUED);
-	final String mannufacturerId = request.getParameter(PARAMETER_MANNUFACTURER_ID);
-
-	try {
-	    final UpdateComputerDTO.UpdateComputerDTOBuilder builder = UpdateComputerDTO.builder().name(name);
-	    builder.introduced(MapperUtils.parseDate(introduced));
-	    builder.discontinued(MapperUtils.parseDate(discontinued));
-	    builder.mannufacturerId(MapperUtils.parseId(mannufacturerId));
-	    builder.id(MapperUtils.parseId(id));
-	    final UpdateComputerDTO dto = builder.build();
-	    // updateComputerValidator.check(dto);
-	    final Computer computer = updateComputerDTOToComputerMapper.map(dto);
+	final ModelAndView modelAndView = new ModelAndView(EDIT_COMPUTER_JSP);
+	if (!result.hasErrors()) {
+	    final Computer computer = updateComputerDTOToComputerMapper.map(computerDTO);
 	    computerService.update(computer);
-	    request.setAttribute(PARAMETER_SUCCESS, true);
-	    doGet(request, response);
-	} catch (RuntimeException e) {
-	    request.setAttribute(PARAMETER_SUCCESS, false);
-	    request.setAttribute(PARAMETER_ID, id);
-	    request.setAttribute(PARAMETER_COMPUTER_NAME, name);
-	    request.setAttribute(PARAMETER_INTRODUCED, introduced);
-	    request.setAttribute(PARAMETER_DISCONTINUED, discontinued);
-	    request.setAttribute(PARAMETER_MANNUFACTURER_ID, mannufacturerId);
-	    // request.setAttribute(PARAMETER_ERRORS, Collections.singletonMap(e.getField(),
-	    // e.getMessage()));
-	    setParameterCompanies(request);
-	    forwardToJsp(request, response);
 	}
-
+	modelAndView.addObject(PARAMETER_SUCCESS, !result.hasErrors());
+	return modelAndView;
     }
 }
--- src/main/java/com/excilys/cdb/exception/BadArgumentRequestException.java
+++ src/main/java/com/excilys/cdb/exception/BadArgumentRequestException.java
@@ -1,18 +1,22 @@
 package com.excilys.cdb.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BadArgumentRequestException extends RuntimeException {
     public BadArgumentRequestException() {
     }
 
     public BadArgumentRequestException(String message) {
-        super(message);
+	super(message);
     }
 
     public BadArgumentRequestException(String message, Throwable cause) {
-        super(message, cause);
+	super(message, cause);
     }
 
     public BadArgumentRequestException(Throwable cause) {
-        super(cause);
+	super(cause);
     }
 }
