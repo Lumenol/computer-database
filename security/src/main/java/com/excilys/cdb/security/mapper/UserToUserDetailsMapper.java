7e9a6bd708453b8d2ce6a1a5455b23b625b2813a
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-06-17 20:29:00 +0200

    Hierarchy role

--- security/src/main/java/com/excilys/cdb/security/mapper/UserToUserDetailsMapper.java
+++ security/src/main/java/com/excilys/cdb/security/mapper/UserToUserDetailsMapper.java
@@ -3,21 +3,25 @@ package com.excilys.cdb.security.mapper;
 import com.excilys.cdb.model.User;
 import com.excilys.cdb.shared.mapper.Mapper;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Component
 public class UserToUserDetailsMapper implements Mapper<User, UserDetails> {
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
     @Override
     public UserDetails map(User user) {
         return new UserDetails() {
 
             @Override
             public Collection<? extends GrantedAuthority> getAuthorities() {
-                return Collections.emptyList();
+                return user.getRoles().stream().map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role.name())).collect(Collectors.toList());
             }
 
             @Override
