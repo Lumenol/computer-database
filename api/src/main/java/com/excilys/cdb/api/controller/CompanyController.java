5db7fa18a85cfc408677e08f375856c2674097b8
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-06-25 12:34:49 +0200

    create/Update Company et acces admin

--- api/src/main/java/com/excilys/cdb/api/controller/CompanyController.java
+++ api/src/main/java/com/excilys/cdb/api/controller/CompanyController.java
@@ -4,18 +4,23 @@ import com.excilys.cdb.api.exception.ResourceNotFound;
 import com.excilys.cdb.model.Company;
 import com.excilys.cdb.service.service.CompanyService;
 import com.excilys.cdb.shared.dto.CompanyDTO;
+import com.excilys.cdb.shared.dto.CreateCompanyDTO;
+import com.excilys.cdb.shared.dto.UpdateCompanyDTO;
 import com.excilys.cdb.shared.mapper.Mapper;
 import com.excilys.cdb.shared.pagination.Page;
 import com.excilys.cdb.shared.pagination.PagingParameters;
 import com.excilys.cdb.shared.utils.Utils;
+import com.excilys.cdb.shared.validator.Validator;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,11 +31,19 @@ public class CompanyController {
     private final CompanyService companyService;
     private final Mapper<Company, CompanyDTO> companyToDto;
     private final PagingParameters pagingParameters;
+    private final Mapper<CreateCompanyDTO, Company> createCompanyDTOCompanyMapper;
+    private final Mapper<UpdateCompanyDTO, Company> updateCompanyDTOCompanyMapper;
+    private final Validator<UpdateCompanyDTO> updateCompanyDTOValidator;
+    private final Validator<CreateCompanyDTO> createCompanyDTOValidator;
 
-    public CompanyController(CompanyService companyService, Mapper<Company, CompanyDTO> companyToDto, PagingParameters pagingParameters) {
+    public CompanyController(CompanyService companyService, Mapper<Company, CompanyDTO> companyToDto, PagingParameters pagingParameters, Mapper<CreateCompanyDTO, Company> createCompanyDTOCompanyMapper, Mapper<UpdateCompanyDTO, Company> updateCompanyDTOCompanyMapper, Validator<UpdateCompanyDTO> updateCompanyDTOValidator, Validator<CreateCompanyDTO> createCompanyDTOValidator) {
         this.companyService = companyService;
         this.companyToDto = companyToDto;
         this.pagingParameters = pagingParameters;
+        this.createCompanyDTOCompanyMapper = createCompanyDTOCompanyMapper;
+        this.updateCompanyDTOCompanyMapper = updateCompanyDTOCompanyMapper;
+        this.updateCompanyDTOValidator = updateCompanyDTOValidator;
+        this.createCompanyDTOValidator = createCompanyDTOValidator;
     }
 
     @GetMapping(params = {"!page", "!size"})
@@ -91,6 +104,31 @@ public class CompanyController {
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable long id) {
         companyService.delete(id);
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody CreateCompanyDTO dto) throws BindException {
+        final BindException errors = new BindException(dto, "company");
+        createCompanyDTOValidator.validate(dto, errors);
+        if (errors.hasErrors()) {
+            throw errors;
+        }
+        final Company company = createCompanyDTOCompanyMapper.map(dto);
+        companyService.create(company);
+
+        final URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(company.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody UpdateCompanyDTO dto) throws BindException {
+        final BindException errors = new BindException(dto, "company");
+        updateCompanyDTOValidator.validate(dto, errors);
+        if (errors.hasErrors()) {
+            throw errors;
+        }
+        companyService.update(updateCompanyDTOCompanyMapper.map(dto));
     }
 
 }
